5615d395fdb06a969f1113ee2d52885374006b2d
yunjichoi9151 <cyjcode99@gmail.com>
2022-03-05 18:54:40 +0900

    Add chapter 2(S02) file

---
+++ 2-2.c
@@ -0,0 +1,12 @@
+// 난생처음 C언어 프로그래밍
+// Chapter 02(Section 02)
+
+#include <stdio.h>
+
+int main() // 한 줄 주석입니다.
+{
+    /* 여러 줄 주석으로 이런 식으로 코드를 무효 처리할 수 있습니다.
+    printf("난생처음 C프로그래밍");
+    */
+    return 0;
+}
