5ec642bac0367255bf3c6e30f485b17a4a1271b7
yunjichoi9151 <cyjcode99@gmail.com>
2022-03-09 00:10:04 +0900

    Add chapter 4 file

---
+++ AC 4.c
@@ -0,0 +1,22 @@
+// 난생처음 C언어 프로그래밍
+// Chapter 04
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int compare(int x, int y)
+{
+    int r = x - y < 0 ? -1 : x == y ? 0 : 1;
+    return r;
+}
+
+int main()
+{
+    int x, y;
+    printf("두 정수를 공백으로 구분하여 입력하세요.\r\n");
+
+    scanf("%d%d", &x, &y);
+
+    int c = compare(x, y);
+    printf(c < 0 ? "%d < %d" : c == 0 ? "%d == %d" : "%d > %d", x, y);
+}
