4ad6e7cb0d99160600a0d7df79bc75d98f47dddc
yunjichoi9151 <cyjcode99@gmail.com>
2022-03-11 23:33:56 +0900

    Add chapter 6(S01) file

---
+++ LAB 6-1.c
@@ -0,0 +1,23 @@
+// 난생처음 C언어 프로그래밍
+// Chapter 06(Section 01)
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+void Print(int count)
+{
+    if(count > 0)
+    {
+        printf("난생처음 ");
+        Print(count - 1);
+    }
+}
+
+int main()
+{
+    int n;
+    printf("반복 횟수를 입력하세요!\r\n");
+    scanf("%d, &n");
+
+    Print(n);
+}
