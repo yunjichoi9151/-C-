0e0d17f11c503ef4aa03207f8f00fb3b8611c703
yunjichoi9151 <cyjcode99@gmail.com>
2022-03-14 23:52:08 +0900

    Add chapter 8 file

---
+++ AC 8.c
@@ -0,0 +1,20 @@
+// 난생처음 C언어 프로그래밍
+// Chapter 08
+
+#include <stdio.h>
+
+int Sum(int index, int arr[])
+{
+    if(index == 0)
+        return arr[0];
+    
+    return arr[index] + Sum(index - 1, arr);
+}
+
+int main()
+{
+    int arr[] = { 3, 5, 1, 10, 9, 8, 2, 6, 4, 7 };
+
+    int count = sizeof(arr) / sizeof(arr[0]);
+    printf("Sum: %d", Sum(count - 1, arr));
+}
